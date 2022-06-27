4c123936496515dc43db490420343015b73447b6
BraveBer <suwenw2@gmail.com>
2022-06-27 04:15:50 -0500

    Add styles to new Typography

--- golds_gym/src/components/HeroBanner.js
+++ golds_gym/src/components/HeroBanner.js
@@ -10,10 +10,14 @@ const HeroBanner = () => {
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
-      <Typography>
+      <Typography fontWeight={700}
+      sx={{ fontSize: { lg: '44px', xs: '40px'}
+      }}
+      >
         Sweat, Smile <br /> and Repeat
       </Typography>
-      <Typography>
+      <Typography fontSize="22px"
+      LineHeight="35px">
         Check out the most effective exercises
       </Typography>
     </Box>
