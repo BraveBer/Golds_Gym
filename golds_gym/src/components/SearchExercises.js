93f4261f85a604271a97109b31f8794d13818f8b
BraveBer <suwenw2@gmail.com>
2022-06-27 14:42:33 -0500

    Add prompt in SearchExercises.js
    Create the Typography with content as the search prompt and add styling to it.

--- golds_gym/src/components/SearchExercises.js
+++ golds_gym/src/components/SearchExercises.js
@@ -5,7 +5,12 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 const SearchExercises = () => {
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-
+      <Typography fontWeight={700} sx={{
+        fontSize: { lg: '44px', xs: '30px'}}}
+        mb="50px" textAlign="center">
+        Awesome Exercises You <br />
+        Should Know
+      </Typography>
     </Stack>
   )
 }
