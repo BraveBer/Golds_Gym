e5c6ab69f4f1e658e404ad646f606c415b3eb0fe
BraveBer <suwenw2@gmail.com>
2022-06-27 14:59:20 -0500

    Add search button

--- golds_gym/src/components/SearchExercises.js
+++ golds_gym/src/components/SearchExercises.js
@@ -19,7 +19,7 @@ const SearchExercises = () => {
               border: 'none',
               borderRadius: '4px'
             },
-            width: { lg: '1170px', xs: '350px'}
+            width: { lg: '800px', xs: '350px'}
             ,
             backgroundColor: '#fff',
             borderRadius: '40px'
@@ -30,6 +30,20 @@ const SearchExercises = () => {
           placeholder="Search Exercises"
           type="text"
         />
+        <Button className="search-btn"
+          sx={{
+            bgcolor:'#FF2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px'},
+            fontSize: { lg: '20px', xs: '14px'}
+            ,
+            height: '56px',
+            position: "absolute"
+          }}
+          >
+          Search
+        </Button>
       </Box>
     </Stack>
   )
