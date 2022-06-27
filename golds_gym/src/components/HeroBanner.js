704ffe6961c3ca9ac036ed517f5a9636e131047d
BraveBer <suwenw2@gmail.com>
2022-06-27 04:07:49 -0500

    Modify the property of Box
    Set up the mt & ml for the outer Box, as well as the position and padding.

--- golds_gym/src/components/HeroBanner.js
+++ golds_gym/src/components/HeroBanner.js
@@ -3,7 +3,10 @@ import { Box, Stack, Typography } from '@mui/material';
 
 const HeroBanner = () => {
   return (
-    <Box>
+    <Box sx={{
+      mt: { lg: '212px', xs: '70px'},
+      ml: { sm: '50px'}
+    }} position="relative" p="20px">
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
