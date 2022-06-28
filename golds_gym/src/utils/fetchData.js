689f796dbfe8aa00574ae25d3b57862a274b9242
BraveBer <suwenw2@gmail.com>
2022-06-28 10:25:45 -0500

    Create the exerciseOptions function
    Use ExerciseDB from Rapid Api and copy & paste the options function to fetchData.js and then delete url which is not needed.

--- golds_gym/src/utils/fetchData.js
+++ golds_gym/src/utils/fetchData.js
@@ -1,3 +1,11 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'ba9826b3f4msh23154c271f02992p12d2f9jsn5b12020e2369',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
