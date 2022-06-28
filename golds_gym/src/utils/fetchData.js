a58b5d6d99d5b60cc371a358a4cebaa3e5d2ed69
BraveBer <suwenw2@gmail.com>
2022-06-28 10:32:15 -0500

    Make key to the api private
    Create a env file to hold the api key for exercisesDB

--- golds_gym/src/utils/fetchData.js
+++ golds_gym/src/utils/fetchData.js
@@ -1,8 +1,8 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'ba9826b3f4msh23154c271f02992p12d2f9jsn5b12020e2369',
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
     }
   };
 
