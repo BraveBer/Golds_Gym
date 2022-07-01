3262d6f1e3bce9b15674be9cf8fe0899cb5cc8a4
BraveBer <suwenw2@gmail.com>
2022-07-01 02:14:18 -0500

    Add content to the Horizontal Scrollbar box

--- golds_gym/src/components/HorizontalScrollbar.js
+++ golds_gym/src/components/HorizontalScrollbar.js
@@ -5,8 +5,13 @@ const HorizontalScrollbar = ({ data }) => {
   return (
     <div>
       {data.map((item) => (
-          <Box>
-
+          <Box
+            key={item.id || item}
+            itemId={item.id || item}
+            title={item.id || item}
+            m="0 40px"
+          >
+            {item}
           </Box>
         )
       )}
