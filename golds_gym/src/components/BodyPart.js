df4c5482e1584a3a72dc349714558ed176d9f64e
BraveBer <suwenw2@gmail.com>
2022-07-01 19:29:24 -0500

    Set properties of the Body Part structure

--- golds_gym/src/components/BodyPart.js
+++ golds_gym/src/components/BodyPart.js
@@ -3,9 +3,23 @@ import { Stack, Typography } from '@mui/material';
 
 import Icon from '../assets/icons/gym.png';
 
-const BodyPart = () => {
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
-    <Stack>
+    <Stack
+        type="button"
+        alighItems="center"
+        justifyContent="center"
+        className="bodyPart-card"
+        sx={{
+            borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+            backgroundColor: '#fff',
+            borderBottomLeftRadius: '20px',
+            width: '270px',
+            height: '280px',
+            cursor: 'pointer',
+            gap: '47px'
+        }}
+    >
         <img src={Icon} alt="dumbbell" styles={{ width: '40px', height: '40px'}} />
     </Stack>
   )
