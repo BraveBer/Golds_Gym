f0e3220d6443d37267ef654c77f50cf3ba70914b
BraveBer <suwenw2@gmail.com>
2022-07-02 17:10:54 -0500

    Finish Pignation

--- golds_gym/src/components/Exercises.js
+++ golds_gym/src/components/Exercises.js
@@ -6,6 +6,20 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: 'smooth'})
+
+  }
+
   return (
     <Box id="exercises"
       sx={{mt: { lg: '110px' }}}
@@ -17,7 +31,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       </Typography>
       <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px'}}}
         flexWrap="wrap" justifyContent="center">
-          {exercises.map((exercise, index) => (
+          {currentExercises.map((exercise, index) => (
             <ExerciseCard key={index} exercise={exercise} />
           ))}
       </Stack>
@@ -27,9 +41,9 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
             color="standard"
             shape="rounded"
             defaultPage={1}
-            count={Math.ceil(exercises.length / 9)}
-            //page={currentPage}
-            //onChange={paginate}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
             size="large"
           />
         )}
