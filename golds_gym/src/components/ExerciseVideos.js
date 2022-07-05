20d77b83bdb476912cbeb39920ab30d139768664
BraveBer <suwenw2@gmail.com>
2022-07-05 12:11:25 -0500

    Import in ExerciseVideos.js

--- golds_gym/src/components/ExerciseVideos.js
+++ golds_gym/src/components/ExerciseVideos.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material';
 
-const ExerciseVideos = () => {
+const ExerciseVideos = ({ exerciseVideos, name }) => {
   return (
     <div>ExerciseVideos</div>
   )
