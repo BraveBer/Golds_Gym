fea93132af18fe368822888f735911e707b1866c
BraveBer <suwenw2@gmail.com>
2022-07-05 12:28:06 -0500

    Fix another possible bug for loading videos

--- golds_gym/src/pages/ExerciseDetail.js
+++ golds_gym/src/pages/ExerciseDetail.js
@@ -22,7 +22,7 @@ const ExerciseDetail = () => {
       setExerciseDetail(exerciseDetailData);
 
       const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions);
-      setExerciseVideos(exerciseVideosData);
+      setExerciseVideos(exerciseVideosData.contents);
 
     }
     
