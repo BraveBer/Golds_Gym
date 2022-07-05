d11a62dacab94a663587948cf8c0db173b849331
BraveBer <suwenw2@gmail.com>
2022-07-05 12:37:13 -0500

    Add exercise name and channel name

--- golds_gym/src/components/ExerciseVideos.js
+++ golds_gym/src/components/ExerciseVideos.js
@@ -22,6 +22,12 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
                     rel="noreferrer"
                 >
                     <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+                    <Typography variant="h5" color="#000">
+                        {item.video.title}
+                    </Typography>
+                    <Typography variant="h6" color="#000">
+                        {item.video.channelName}
+                    </Typography>
                 </a>
             ))}
         </Stack>
